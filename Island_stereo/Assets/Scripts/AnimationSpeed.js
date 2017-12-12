c97e50f38e4ff5454a7634596de714046da5dbfc
Yuzhou Huang <happyjoe2008@gmail.com>
2017-12-11 20:38:10 -0600

    implemented SteamVR in final project

--- Island_stereo/Assets/Scripts/AnimationSpeed.js
+++
@@ -1,7 +0,0 @@
-var statename = "defaulto";
-var speed = 0.5;
-
-function Start () 
-{
-	animation[statename].speed = speed;
-}
--- Island_stereo/Assets/quitscript.js
+++
@@ -1,9 +0,0 @@
-function Start() {
-	Screen.showCursor = false;
-}
-
-function Update () { 
-   if (Input.GetKeyDown("q")) {
-		Application.Quit();
-   }
-}
